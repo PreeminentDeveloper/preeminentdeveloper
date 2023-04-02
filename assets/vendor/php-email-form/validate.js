9b43efb2d7606f1d69f3b5aa443fc7a7bbada867
oluwasegun badaru <badaruoluwasegun@gmail.com>
2023-04-02 22:36:06 +0100

    implemented contact form

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -6,7 +6,7 @@
 (function () {
   "use strict";
 
-  let forms = document.querySelectorAll('.php-email-form');
+  let forms = document.querySelectorAll('.pp-email-form');
 
   forms.forEach( function(e) {
     e.addEventListener('submit', function(event) {
